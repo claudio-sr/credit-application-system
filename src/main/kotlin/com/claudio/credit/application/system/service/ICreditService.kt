3c53a9ef4ce18425c68bcf8a399fb5e21d49f650
claudio-sr <jclaudiosiqueira.js@gmail.com>
2023-07-25 14:24:04 -0300

    Implementação das interfaces CustomerService e CreditService

---
+++ src/main/kotlin/com/claudio/credit/application/system/service/ICreditService.kt
@@ -0,0 +1,13 @@
+package com.claudio.credit.application.system.service
+
+import com.claudio.credit.application.system.entity.Credit
+import java.util.UUID
+
+interface ICreditService {
+
+    fun save(credit: Credit): Credit
+
+    fun findAllByCustomer(customerId: Long): List<Credit>
+
+    fun findByCreditCode(customerId: Long, creditCode: UUID): Credit
+}
