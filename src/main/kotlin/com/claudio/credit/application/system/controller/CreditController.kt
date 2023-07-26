e9e8e328d136536bac22992a3cafc6842b85491d
claudio-sr <jclaudiosiqueira.js@gmail.com>
2023-07-26 11:34:31 -0300

    Endpoints CreditController - (findAllCustomerCredits)

--- src/main/kotlin/com/claudio/credit/application/system/controller/CreditController.kt
+++ src/main/kotlin/com/claudio/credit/application/system/controller/CreditController.kt
@@ -1,12 +1,16 @@
 package com.claudio.credit.application.system.controller
 
 import com.claudio.credit.application.system.dto.CreditDto
+import com.claudio.credit.application.system.dto.CreditViewList
 import com.claudio.credit.application.system.entity.Credit
 import com.claudio.credit.application.system.service.impl.CreditService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.stream.Collectors
 
 @RestController
 @RequestMapping("/api/credits")
@@ -18,5 +22,11 @@ class CreditController(
     fun saveCredit(@RequestBody creditDto: CreditDto): String {
         val credit: Credit = this.creditService.save(creditDto.toEntity())
         return "Credit ${credit.creditCode} - Customer ${credit.customer?.firstName} saved!"
+    }
+
+    @GetMapping
+    fun findAllByCustomer(@RequestParam(value = "customerId") customerId: Long): List<CreditViewList> {
+        return this.creditService.findAllByCustomer(customerId)
+            .stream().map { credit: Credit -> CreditViewList(credit) }.collect(Collectors.toList())
     }
 }
