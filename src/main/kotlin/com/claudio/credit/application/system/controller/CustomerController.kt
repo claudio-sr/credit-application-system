df3205679b4773090ae211fd051b001b404d2b7d
claudio-sr <jclaudiosiqueira.js@gmail.com>
2023-07-26 10:04:16 -0300

    Endpoints CustomerController - (findById, delete)

--- src/main/kotlin/com/claudio/credit/application/system/controller/CustomerController.kt
+++ src/main/kotlin/com/claudio/credit/application/system/controller/CustomerController.kt
@@ -1,7 +1,12 @@
 package com.claudio.credit.application.system.controller
 
 import com.claudio.credit.application.system.dto.CustomerDto
+import com.claudio.credit.application.system.dto.CustomerView
+import com.claudio.credit.application.system.entity.Customer
 import com.claudio.credit.application.system.service.impl.CustomerService
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,4 +21,13 @@ class CustomerController(private val customerService: CustomerService) {
         val savedCustomer = this.customerService.save(customerDto.toEntity())
         return "Customer ${savedCustomer.email} saved!"
     }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Long) : CustomerView {
+        val customer: Customer = this.customerService.findById(id)
+        return CustomerView(customer)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteCustomer(@PathVariable id: Long) = this.customerService.delete(id)
 }
